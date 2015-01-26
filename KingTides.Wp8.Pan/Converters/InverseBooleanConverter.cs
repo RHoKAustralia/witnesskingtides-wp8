c33c7c1b8cbabcc36bb3bc88fd6a9956892cb816
Shaun Wilde <shaun_wilde@hotmail.com>
2015-01-26 16:25:57 +1100

    address image chopping, improve wait cursor behaviour

--- KingTides.Wp8.Pan/Converters/InverseBooleanConverter.cs
+++ KingTides.Wp8.Pan/Converters/InverseBooleanConverter.cs
@@ -1,15 +1,14 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows.Data;
 
 namespace KingTides.Wp8.Pan.Converters
 {
-    //[ValueConversion(typeof(bool), typeof(bool))]
     public class InverseBooleanConverter : IValueConverter
     {
         #region IValueConverter Members
 
-        public object Convert(object value, Type targetType, object parameter,
-            System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (targetType != typeof(bool))
                 throw new InvalidOperationException("The target must be a boolean");
@@ -17,10 +16,12 @@ namespace KingTides.Wp8.Pan.Converters
             return !(bool)value;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter,
-            System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotSupportedException();
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
         }
 
         #endregion
